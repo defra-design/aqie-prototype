3b699df29c1db52625b2b8a879a8bf52679cc848
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-06-28 14:05:47 +0100

    Remove action plan

--- app/routes.js
+++ app/routes.js
@@ -4,15 +4,10 @@
 //
 
 const govukPrototypeKit = require('govuk-prototype-kit')
-const actionPlanController = require('./controllers/action-plan.js')
 const homeController = require('./controllers/home.js')
 const notificationsController = require('./controllers/notifications.js')
 
 const router = govukPrototypeKit.requests.setupRouter()
-
-// Action plan journey
-router.all('/action-plan/:view?', actionPlanController.all)
-router.post('/action-plan/:view?', actionPlanController.post)
 
 // Home page
 router.get('/home', homeController.get)
